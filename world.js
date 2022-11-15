2d01e2179634c68635594cc40efd042f549dc7e3
Rajahni Cunningham <rajahni.cunningham@mymona.uwi.edu>
2022-11-14 23:35:55 -0500

    Final code

--- world.js
+++ world.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
     let lookupBtn = $("#lookup");
     let resultDiv = $("#result");
+    let citiesBtn = $("#lookupCity")
+
     resultDiv.hide();
 
     lookupBtn.click(function(){
         let search = $("#country").val().trim();
-        $.get(`http://localhost/info2180-lab5/world.php?country=${search}&context=none`, function(result){
-            resultDiv.html(result).slideDown();
+        $.get(`http://localhost/info2180-lab5/world.php?country=${search}&context=none`, function(result) {
+            resultDiv.html(result).show();
+        })
+    })
+    
+    citiesBtn.click(function(){
+        let search = $("#country").val().trim();
+        $.get(`http://localhost/info2180-lab5/world.php?country=${search}&context=cities`, function(result) {
+            resultDiv.html(result).show();
         })
     })
 });
